822c18a2d0e820559f8d442d895b6321d4fe7b5e
Raj Coderx <>
2020-03-06 18:27:30 +0530

    Updates

---
+++ precache-manifest.b41a95b174e75b80224b3cc473917721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9eb5dfda278c797bf4d42b040ecc7dc",
+    "url": "/gw-proxy-serverless-wireframe3/index.html"
+  },
+  {
+    "revision": "d5d1b953af7bf97987f5",
+    "url": "/gw-proxy-serverless-wireframe3/static/css/2.156fe21f.chunk.css"
+  },
+  {
+    "revision": "8de49f3332d03d2dfb4b",
+    "url": "/gw-proxy-serverless-wireframe3/static/css/main.55415b9c.chunk.css"
+  },
+  {
+    "revision": "d5d1b953af7bf97987f5",
+    "url": "/gw-proxy-serverless-wireframe3/static/js/2.bf210d36.chunk.js"
+  },
+  {
+    "revision": "aa762385ab423a29206547471fcc4bac",
+    "url": "/gw-proxy-serverless-wireframe3/static/js/2.bf210d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de49f3332d03d2dfb4b",
+    "url": "/gw-proxy-serverless-wireframe3/static/js/main.3a2a3c20.chunk.js"
+  },
+  {
+    "revision": "8f3d440165c1304604df",
+    "url": "/gw-proxy-serverless-wireframe3/static/js/runtime-main.0134bb96.js"
+  }
+]);
